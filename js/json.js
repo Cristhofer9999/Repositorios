f48dd7253c6b1dfd52d87b5f69a744faa0155225
Cristhofer9999 <129204967+Cristhofer9999@users.noreply.github.com>
2023-05-04 11:54:09 -0600

    Validacion de CurP CON JQUERY

--- js/json.js
+++ js/json.js
@@ -5,13 +5,26 @@ $(function(){
   $("#boton_validar_curp").click(function(){
     
      //$("#boton_validar_curp").hide();
-     if($("#inputCurp").val().indexOf('@', 0) == -1 || $("#inputCurp").val().indexOf('.', 0) == -1) {
-      alert('El correo electrónico introducido no es correcto.');
-      return false;
-  }
+     //if($("#inputCurp").val().indexOf('@', 0) == -1 || $("#inputCurp").val().indexOf('.', 0) == -1) {
+      if($("#inputCurp").val().match(/^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/)) {
+      
+      alert('Formato de curp correcto. ');
+      return true;
 
-  alert('El email introducido es correcto ¡Felicidades!.');
-    //alert("Hola");
+       
+     
+
+    
+
+      }
+     else{
+      
+      alert('Formato de curp incorrecto');
+     //alert("Hola"); 
+     }
+   
+    
+
   });
 
 });
